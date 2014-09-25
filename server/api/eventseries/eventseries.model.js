bac5933ef69a8bd22b633017189e937c8bdfb538
Thomas Pani <thomas.pani@gmail.com>
2014-09-25 16:41:08 +0200

    Add admin and editor collections to EventSeries model

--- server/api/eventseries/eventseries.model.js
+++ server/api/eventseries/eventseries.model.js
@@ -7,6 +7,8 @@ var uuid = require('node-uuid'),
 var EventseriesSchema = new Schema({
   name: String,
   info: String,
+  u_admins: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  u_editors: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   cal_pub_id: { type: String, default: uuid.v4 }
 });
 
